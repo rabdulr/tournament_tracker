111218dbb23ccf0d806c5f9a882abf8f56d0b9fb
Redzuan <rabdulr@icloud.com>
2020-08-17 13:21:58 -0700

    First commit

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Represents one team in the matchup
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represents the score of this particular team
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// Represents the matcup that this team came
+        /// from as the winner.
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
